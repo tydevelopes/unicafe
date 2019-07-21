69cd78c14c53877e1ddc84a783a896b4bab75723
Emmanuel <eadjeifrimpong@worcester.edu>
2019-07-21 03:08:01 -0400

    first refactor for step 3 and 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Statistics = ({ good, neutral, bad }) => {
+  // calcutions
+  const totalClicks = good + neutral + bad;
+  const average = totalClicks && (good - bad) / totalClicks;
+  const positive = totalClicks && (good / totalClicks) * 100;
+  const renderStats = totalClicks ? (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {totalClicks}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </>
+  ) : (
+    'No feedback given'
+  );
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      {renderStats}
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
@@ -13,24 +39,13 @@ const App = () => {
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
 
-  // calcutions
-  const totalClicks = good + neutral + bad;
-  const average = totalClicks && (good - bad) / totalClicks;
-  const positive = totalClicks && (good / totalClicks) * 100;
-
   return (
     <div>
       <h2>Give feedback</h2>
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {totalClicks}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
